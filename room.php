2bc952663e552f35afb43660510fe8ab1ea5a2a4
Joselu <joseluissgz97@gmail.com>
2024-06-13 22:47:24 +0200

    feat:con sult to ger all rooms

--- room.php
+++ room.php
@@ -1,3 +1,24 @@
 <?php
 require_once('./setup.php');
 echo $blade->run("room", ["name" => "Joselu"]);
+$db = conectDB();
+$query = "SELECT 
+r._id, 
+ri.image AS images,
+r.room_number,
+r.room_type,
+r.description,
+r.offer,
+r.offer_price,
+r.status,
+a.name 
+FROM
+room r
+JOIN
+room_images ri ON r._id=ri.room_id
+JOIN room_amenities ra ON r._id=ra.room_id
+JOIN amenities a ON a.id = ra.amenity_id;
+";
+
+
+$result = mysqli_query($db, $query);
