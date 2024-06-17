2e9b2c7200bbfede6aeb1d845506fce52e2b9a1a
Joselu <joseluissgz97@gmail.com>
2024-06-17 23:12:15 +0200

    feat: Add room details page

---
+++ views/room_details.blade.php
@@ -0,0 +1,23 @@
+@extends('layout')
+
+@section('content')
+<section class="home-component">
+    <div class="home-component__content rooms-list">
+        <div class="wrapper">
+            <div class="room-home">
+                <h2 class="home-component__subtitle title">The Ultimate Luxury</h2>
+                <h2 class="home-component__title old">ROOM DETAILS</h2>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="wrapper2">
+    <button class="toogleRoom">ROOM DETAILS</button>
+    <section class="rooms-grid">
+
+    </section>
+</section>
+
+
+
+@endsection
