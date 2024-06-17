d148149f4bfd03fb442a0dc62475d64c51a7901a
Joselu <joseluissgz97@gmail.com>
2024-06-17 23:11:39 +0200

    feat: get all Rooms for swipper

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
 <?php
 require_once('./setup.php');
+require_once('./querys/room.querys.php');
 conectDB();
-echo $blade->run("index", ["name" => "Joselu"]);
+
+$db = conectDB();
+
+$result = mysqli_query($db, $queries['query_allrooms']);
+
+$rooms = array();
+
+
+while ($row = $result->fetch_assoc()) {
+    if (!isset($rooms[$row['_id']])) {
+        $rooms[$row['_id']] = $row;
+        $rooms[$row['_id']]['amenities'] = array($row['amenities']);
+    } else {
+        if (!in_array($row['amenities'], $rooms[$row['_id']]['amenities'])) {
+            $rooms[$row['_id']]['amenities'][] = $row['amenities'];
+        }
+    }
+}
+echo $blade->run("index", ["rooms" => $rooms]);
