7525ccf7a5400b7b0b79f1b99fdd369a0d2fae38
Joselu <joseluissgz97@gmail.com>
2024-06-18 17:59:33 +0200

    feat: Update room_details.php to fetch and display room details and handle room reservation

--- room_details.php
+++ room_details.php
@@ -2,7 +2,7 @@
 require_once('./setup.php');
 $db = conectDB();
 
-$room_id = $_GET['room'] ?? null;
+$room_id = (int) ($_GET['room'] ?? null);
 
 if (!$room_id) {
     die('No se proporcionó un ID de habitación válido.');
@@ -24,17 +24,66 @@ room r
 JOIN
 room_images ri ON r._id=ri.room_id
 JOIN room_amenities ra ON r._id=ra.room_id
-JOIN amenities a ON a.id = ra.amenity_id,
+JOIN amenities a ON a.id = ra.amenity_id
 WHERE
 r._id =$room_id";
 
-// $result = mysqli_query($db, $query_OnRoom);
-// if ($result) {
-//     echo '<pre>';
-//     print_r($result);
-//     echo '</pre>';
-// } else {
-//     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-// }
+$result = mysqli_query($db, $query_OnRoom);
 
-echo $blade->run("room_details", ['room_id' => $room_id]);
+$roomdetails = array();
+
+$roomdetails = [];
+$images = [];
+$amenities = [];
+
+while ($row = mysqli_fetch_assoc($result)) {
+    if (empty($roomdetails)) {
+        $roomdetails = [
+            '_id' => $row['_id'],
+            'room_number' => $row['room_number'],
+            'room_type' => $row['room_type'],
+            'description' => $row['description'],
+            'offer' => $row['offer'],
+            'offer_price' => $row['offer_price'],
+            'price' => $row['price'],
+            'status' => $row['status']
+        ];
+    }
+    if (!in_array($row['images'], $images)) {
+        $images[] = $row['images'];
+    }
+    if (!in_array($row['amenities'], $amenities)) {
+        $amenities[] = $row['amenities'];
+    }
+}
+
+$roomdetails['images'] = $images;
+$roomdetails['amenities'] = $amenities;
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $gues_name = $_POST['name'] ?? '';
+    $gues_lastName = $_POST['last_name'] ?? '';
+
+    $reservation_id = uniqid();
+    $order_date = date('Y-m-d');
+
+    $check_in = $_POST['check_in'] ?? '';
+    $check_out = $_POST['check_out'] ?? '';
+    $room_id = $roomdetails['_id'];
+    $room_type = $roomdetails['room_type'];
+    $special_request = $_POST['special_request'] ?? '';
+    $status = 'pending';
+
+    $insertQuery = "INSERT INTO booking 
+    (gues_name, gues_lastName, reservation_id, order_date, check_in, check_out, room_number, room_id, room_type, specialRequest, status) 
+    VALUES 
+    ('$gues_name', '$gues_lastName', '$reservation_id', '$order_date', '$check_in', '$check_out', {$roomdetails['room_number']}, {$roomdetails['_id']}, '{$roomdetails['room_type']}', '$special_request', '$status')";
+
+    if (mysqli_query($db, $insertQuery)) {
+        echo "Reserva creada exitosamente.";
+    } else {
+        echo "Error al crear la reserva: " . mysqli_error($db);
+    }
+}
+
+echo $blade->run("room_details", ['roomdetails' => $roomdetails]);
