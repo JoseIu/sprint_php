37d32def96c2b7b342aaf1bf6cf0868729dd0c43
Joselu <joseluissgz97@gmail.com>
2024-06-18 18:34:03 +0200

    feat: Add function to check room availability and handle room reservation

--- index.php
+++ index.php
@@ -20,4 +20,55 @@ while ($row = $result->fetch_assoc()) {
         }
     }
 }
+function getBookins()
+{
+    $bookings = array();
+    global $db;
+    $booking_querry = "SELECT * FROM bookings";
+    $result = mysqli_query($db, $booking_querry);
+
+    return $result;
+}
+
+
+function isOccupied($startDate, $endDate)
+{
+    global $db;
+
+    $startDate = date('Y-m-d', strtotime($startDate));
+    $endDate = date('Y-m-d', strtotime($endDate));
+
+    $booking_query = "
+        SELECT COUNT(*) AS total_bookings 
+        FROM booking
+        WHERE check_in < '$endDate' 
+        AND check_out > '$startDate'
+    ";
+
+    $result = mysqli_query($db, $booking_query);
+
+    if ($result) {
+        $row = mysqli_fetch_assoc($result);
+        $totalBookings = (int) $row['total_bookings'];
+        if ($totalBookings === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        die('Error en la consulta: ' . mysqli_error($db));
+    }
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $inital_date = $_POST['arrival'] ?? '';
+    $end_date = $_POST['departure'] ?? '';
+
+    $isOcupiedd = isOccupied($inital_date, $end_date);
+    if ($isOcupiedd) {
+        echo "La habitacion esta disponible";
+    } else {
+        echo "La habitacion no esta disponible";
+    }
+}
 echo $blade->run("index", ["rooms" => $rooms]);
