aefccb9fffde74120c6123043f256a1bc4c2ddc3
Joselu <joseluissgz97@gmail.com>
2024-06-20 20:33:27 +0200

    feat: Update room display options, add room reservation functionality

--- index.js
+++ index.js
@@ -1,48 +1,73 @@
-'use strict';
-const toggleActive = document.querySelector('.header__toggle');
-const toggleClose = document.querySelector('.header__close');
-const navMenu = document.querySelector('.header__nav');
+"use strict";
+const toggleActive = document.querySelector(".header__toggle");
+const toggleClose = document.querySelector(".header__close");
+const navMenu = document.querySelector(".header__nav");
 
-const roomListContainer = document.querySelector('.rooms-grid');
-const roomsList = document.querySelectorAll('.room');
-const toogleRoom = document.querySelector('.toogleRoom');
+const roomListContainer = document.querySelector(".rooms-grid");
+const roomsList = document.querySelectorAll(".room");
+const toogleRoomList = document.querySelector(".toogle-list");
+const toogleRoomGrid = document.querySelector(".toogle-grid");
 
 const toggleMenu = (type) => {
-  if (type === 'open') {
-    navMenu.classList.add('header__nav--active');
-  }
-  if (type === 'close') return navMenu.classList.remove('header__nav--active');
+	if (type === "open") {
+		toggleClose.classList.remove("header__toggle--hidden");
+		toggleActive.classList.add("header__toggle--hidden");
+
+		navMenu.classList.add("header__nav--active");
+	}
+	if (type === "close") {
+		toggleActive.classList.remove("header__toggle--hidden");
+		toggleClose.classList.add("header__toggle--hidden");
+		return navMenu.classList.remove("header__nav--active");
+	}
 };
 
-if (toogleRoom) {
-  toogleRoom.addEventListener('click', () => {
-    roomListContainer.classList.remove('rooms-grid');
-    roomListContainer.classList.add('rooms-grid--list');
+if (toogleRoomList) {
+	toogleRoomList.addEventListener("click", () => {
+		toogleRoomList.classList.add("rooms__action--active");
+		toogleRoomGrid.classList.remove("rooms__action--active");
 
-    roomsList.forEach((room) => {
-      room.classList.add('room--list');
-    });
-  });
-}
-if (typeof Swiper !== 'undefined') {
-  const swiper = new Swiper('.swiper', {
-    slidesPerView: 1,
-    // Optional parameters
-    direction: 'horizontal',
+		roomListContainer.classList.add("rooms-grid--list");
 
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-      700: {
-        slidesPerView: 2,
-      },
-    },
-  });
+		roomsList.forEach((room) => {
+			room.classList.add("room--list");
+			//div-content
+			room.children[1].classList.add("room__content--list");
+		});
+	});
 }
-document.addEventListener('DOMContentLoaded', () => {
-  toggleActive.addEventListener('click', () => toggleMenu('open'));
-  toggleClose.addEventListener('click', () => toggleMenu('close'));
+if (toogleRoomGrid) {
+	toogleRoomGrid.addEventListener("click", () => {
+		toogleRoomList.classList.remove("rooms__action--active");
+
+		toogleRoomGrid.classList.add("rooms__action--active");
+
+		roomListContainer.classList.remove("rooms-grid--list");
+
+		roomsList.forEach((room) => {
+			room.classList.remove("room--list");
+		});
+	});
+}
+if (typeof Swiper !== "undefined") {
+	const swiper = new Swiper(".swiper", {
+		slidesPerView: 1,
+		// Optional parameters
+		direction: "horizontal",
+
+		// Navigation arrows
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		},
+		breakpoints: {
+			700: {
+				slidesPerView: 2,
+			},
+		},
+	});
+}
+document.addEventListener("DOMContentLoaded", () => {
+	toggleActive.addEventListener("click", () => toggleMenu("open"));
+	toggleClose.addEventListener("click", () => toggleMenu("close"));
 });
